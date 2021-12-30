5223824d15ebbd3d8974b39d8701a6487588cb0d
kaoger <rend579@gmail.com>
2021-12-30 12:27:02 +0800

    change 帳號鎖定

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 password = '1234'
 i = 3
 while i > 0:
+	i = i - 1
 	word = input('請輸入密碼: ')
 	if word == password:
 		print('密碼正確，登入中')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤，還有', i, '次機會')
-		if i == 0:
-			print('密碼鎖定')
-			break
+		print('密碼錯誤')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('帳號鎖定')
